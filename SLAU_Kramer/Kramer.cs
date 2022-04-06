e080127aef290ec918dd4646ae2d52aa18023e70
Edvard_Kenua <Edvard_Kenua@NOTEBOOK>
2022-04-06 18:01:17 +0300

    Добавьте файлы проекта.

---
+++ SLAU_Kramer/Kramer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAU_Kramer
+{
+    public partial class Kramer
+    {
+        double[,] a = new double[3, 3];
+        double[] b = new double[3];
+        public double X, Y, Z;
+        public void GetNums(double[,] A, double[] B)
+        {
+            a = A; b = B; 
+        }
+        private static double Det(double[,] matr)
+        {
+            return matr[0, 0] * matr[1, 1] * matr[2, 2] + matr[0, 1] * matr[1, 2] * matr[2, 0] + matr[0, 2] * matr[1, 0] * matr[2, 1] - matr[0, 2] * matr[1, 1] * matr[2, 0] - matr[0, 1] * matr[1, 0] * matr[2, 2] - matr[0, 0] * matr[1, 2] * matr[2, 1];
+        }
+
+        private static double[,] CopyArr(double[,] matr)
+        {
+            double[,] arr = new double[3, 3];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    arr[i, j] = matr[i, j];
+            return arr;
+        }
+
+        //        public static void Main()
+        //        {
+
+        //            for (int i = 0; i < 3; i++)
+        //            {
+        //                Console.WriteLine($"{i + 1}е уравнение:");
+        //                for (int j = 0; j < 3; j++)
+        //                {
+        //                    Console.Write($"x{i + 1}{j + 1} = ");
+        //                    a[i, j] = Convert.ToDouble(Console.ReadLine());
+        //                }
+        //                Console.WriteLine();
+        //            }
+        //            for (int i = 0; i < 3; i++)
+        //            {
+        //                Console.Write($"b{i + 1} = ");
+        //                b[i] = Convert.ToDouble(Console.ReadLine());
+        //            }
+        public void Calculate()
+        {
+            double[,] x1 = CopyArr(a);
+            double[,] x2 = CopyArr(a);
+            double[,] x3 = CopyArr(a);
+            x1[0, 0] = x2[0, 1] = x3[0, 2] = b[0];
+            x1[1, 0] = x2[1, 1] = x3[1, 2] = b[1];
+            x1[2, 0] = x2[2, 1] = x3[2, 2] = b[2];
+            double dA = Det(a);
+            double dX1 = Det(x1);
+            double dX2 = Det(x2);
+            double dX3 = Det(x3);
+            X = dX1 / dA;
+            Y = dX2 / dA;
+            Z = dX3 / dA;
+
+        }
+        //            double[,] x1 = CopyArr(a);
+        //            double[,] x2 = CopyArr(a);
+        //            double[,] x3 = CopyArr(a);
+        //            x1[0, 0] = x2[0, 1] = x3[0, 2] = b[0];
+        //            x1[1, 0] = x2[1, 1] = x3[1, 2] = b[1];
+        //            x1[2, 0] = x2[2, 1] = x3[2, 2] = b[2];
+        //            double dA = Det(a);
+        //            double dX1 = Det(x1);
+        //            double dX2 = Det(x2);
+        //            double dX3 = Det(x3);
+        //            Console.WriteLine($"x1 = dX1/dA = {dX1} / {dA} = {dX1 / dA}");
+        //            Console.WriteLine($"x2 = dX2/dA = {dX2} / {dA} = {dX2 / dA}");
+        //            Console.WriteLine($"x3 = dX3/dA = {dX3} / {dA} = {dX3 / dA}");
+        //  }
+    }
+}
---
+++ SLAU_Kramer/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SLAU_Kramer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double[,] a = new double[3, 3];
+        double[] b = new double[3];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            a[0, 0] = double.Parse(A01.Text);
+            a[0, 1] = double.Parse(A02.Text);
+            a[0, 2] = double.Parse(A03.Text);
+
+            a[1, 0] = double.Parse(A11.Text);
+            a[1, 1] = double.Parse(A12.Text);
+            a[1, 2] = double.Parse(A13.Text);
+
+            a[2, 0] = double.Parse(A21.Text);
+            a[2, 1] = double.Parse(A22.Text);
+            a[2, 2] = double.Parse(A23.Text);
+
+            b[0] = double.Parse(B0.Text);
+            b[1] = double.Parse(B1.Text);
+            b[2] = double.Parse(B2.Text);
+
+            Kramer kramer = new Kramer();
+            kramer.GetNums(a,b);
+
+            X.Text = Convert.ToString(a[0,1]);
+        }
+    }
+}
